45d2b8844ee9fcb5accc70ee6ff4086a1ccf1bef
tulio <tuliovictorp@hotmail.com>
2024-09-22 12:09:12 -0300

    alt

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -27,7 +27,7 @@ const Main = () => {
         </div>
       </div>
       <div className='content-image'>
-        <img src='/public/logo.png' />
+        <img src='/public/logo.png' alt='logo-turbo' />
       </div>
     </main>
   );
