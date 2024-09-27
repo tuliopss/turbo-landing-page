8a69262a88aeb2d7f7dc031f644530e974ec5564
tulio <tuliovictorp@hotmail.com>
2024-09-27 10:13:37 -0300

    install pixel

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,33 +4,6 @@ const Main = () => {
   const [isHidden, setIsHidden] = useState(true);
 
   return (
-    // <main className='main-container'>
-    //   <h1 className='content-title'>Turbine suas vendas já!</h1>
-    //   <div className='content-container'>
-    //     <div className='text-area'>
-    //       <h4>
-    //         Acelere o crescimento do seu negócio com estratégias de{" "}
-    //         <span className='custom-span'>
-    //           tráfego pago e marketing digital
-    //         </span>{" "}
-    //         personalizadas. Resultados reais, parceria constante.
-    //       </h4>
-
-    //       <p className='text-hidden' hidden={isHidden}>
-    //         Somos especialistas em tráfego pago e estratégias digitais,
-    //         oferecemos soluções personalizadas para aumentar sua visibilidade,
-    //         atrair clientes e gerar resultados reais.
-    //       </p>
-    //       <button className='see-more' onClick={() => setIsHidden(!isHidden)}>
-    //         {isHidden ? "Ler mais " : "Ler menos"}
-    //       </button>
-    //     </div>
-
-    //     <div className='content-image'></div>
-
-    //     <img src='/logo.png' alt='logo-turbo' />
-    //   </div>
-    // </main>
     <main className='main-container'>
       <h1 className='content-title'>Turbine suas vendas já!</h1>
       <div className='content-wrapper'>
